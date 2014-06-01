e30b05aaacb4e5b0af1b9bf12cacf03f60387285
Sam <skx@berkeley.edu>
2014-05-31 21:58:45 -0700

    moved player logic & added network framework

--- js/game.js
+++ js/game.js
@@ -5,6 +5,10 @@ var Game = {
 	engine: null,
 	player: null,
 	treasure: null,
+	database: null,
+	onlineUsersRef: null,
+	userRef: null,
+	connectedRef: null
 	map: {},
 
 	init: function() {
@@ -12,6 +16,24 @@ var Game = {
 		//ROT.DEFAULT_HEIGHT = 50;
 		this._display = new ROT.Display({width: 80, height: 24});
 		var game = this; //questionable
+
+		//Set reference to database
+		this.database = new Firebase('https://treasure.firebaseio.com');
+
+		//Connect to login server
+		this.onlineUsersRef = this.database.child('users');
+		this.userRef = onlineUsersRef.push();
+
+		//Add ourself to the online user list when we are connected
+		this.connectedRef = this.database.child('.info/connected');
+		connectedRef.on("value", function(snap) {
+			if (snap.val()) {
+				userRef.set(true);
+
+				//Remove ourself when we disconnect
+				userRef.onDisconnect().remove();
+			}
+		});
 
 		//We only want the active State to handle input
 		var bindEventToState = function(event) {
@@ -119,70 +141,6 @@ Game.drawWholeMap = function() {
 	}
 };
 
-
-
-
-var Player = function(x, y) {
-	this._x = x;
-	this._y = y;
-	this._draw();
-}
-
-Player.prototype._draw = function() {
-	Game.getDisplay().draw(this._x, this._y, "@", "#ff0");
-}
-
-Player.prototype._checkBox = function() {
-	var key = this._x + "," + this._y;
-	if (Game.map[key] != "*") {
-		Game.getDisplay().draw(this._x, this._y - 2, "There is no chest here.");
-	} else if (key == Game.treasure) {
-		Game.getDisplay().draw(this._x, this._y - 2, "You found treasure.");
-	} else {
-		Game.getDisplay().draw(this._x, this._y - 2, "Nothing in this chest.");
-	}
-}
-
-Player.prototype.act = function() {
-	Game.engine.lock();
-	//waiting for the user to input something
-	//window.addEventListener("keydown", this);
-}
-
-Player.prototype.handleEvent = function(e) {
-	var keyMap = {};
-	keyMap[ROT.VK_UP] = 0;
-	keyMap[ROT.VK_RIGHT] = 1;
-	keyMap[ROT.VK_LEFT] = 3;
-	keyMap[ROT.VK_DOWN] = 2;
-
-	var code = e.keyCode;
-	if (code == ROT.VK_SPACE) {
-		this._checkBox();
-		return;
-	}
-
-	//Only take valid keys
-	if (!(code in keyMap)) {
-		return;
-	}
-
-	//Get the change in x, y after input
-	var diff = ROT.DIRS[4][keyMap[code]];
-	var newX = this._x + diff[0];
-	var newY = this._y + diff[1];
-
-	var newKey = newX + "," + newY;
-	if (!(newKey in Game.map)) { return; }
-
-	Game.getDisplay().draw(this._x, this._y, Game.map[this._x + ",", + this._y]);
-	this._x = newX;
-	this._y = newY;
-	this._draw();
-	window.removeEventListener("keydown", this);
-	Game.engine.unlock();
-
-}
 
 Game._createPlayer = function(freeCells) {
 	var index = Math.floor(ROT.RNG.getUniform() * freeCells.length);
